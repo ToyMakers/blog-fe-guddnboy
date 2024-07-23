3f8d696f6620cdedc72bdb45532afb91afed1800
guddnboy <hung3018@naver.com>
2024-07-23 19:04:31 +0900

    feat(회원가입 페이지에서 입력값 console.log 출력[임시] )

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,43 +1,96 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import useStore from '../store/store';
+import { Url } from 'next/dist/shared/lib/router/router';
 
 const SignUp = () => {
   const router = useRouter();
-  const navigateTo = (path: string) => {
-    router.push(path);
+  const {
+    username,
+    password,
+    passwordConfirm,
+    nickname,
+    introduction,
+    setUsername,
+    setPassword,
+    setPasswordConfirm,
+    setNickname,
+    setIntroduction,
+  } = useStore();
+
+  const navigateTo = (path: Url) => {
+    if (
+      username === '' ||
+      password === '' ||
+      passwordConfirm === '' ||
+      nickname === '' ||
+      introduction === ''
+    ) {
+      alert('모든 항목을 입력해주세요.');
+      return;
+    } else {
+      if (password !== passwordConfirm) {
+        alert('비밀번호가 일치하지 않습니다.');
+        return;
+      }
+      console.log({
+        username,
+        password,
+        passwordConfirm,
+        nickname,
+        introduction,
+      });
+      router.push(path);
+    }
   };
 
   return (
-    <main className={'flex min-h-screen items-center p-24 justify-center'}>
+    <main className="flex min-h-screen items-center p-24 justify-center">
       <div className="flex justify-between">
-        <div className="">
-          <div className="text-2xl font-bold text-center">회원가입</div>
-          <div className="mb-2">
+        <div>
+          <div className="text-2xl h-12 font-bold text-center">회원가입</div>
+          <div className="mb-2 h-12">
             <input
-              className="w-80 mb-2"
+              className="w-80 mb-2 h-12"
               type="text"
-              placeholder="사용하실 아이디를 입력하세요."></input>
+              placeholder="사용하실 아이디를 입력하세요."
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-          <div className="mb-2">
+          <div className="mb-2 h-12">
             <input
-              className="w-80 mb-2"
-              type="text"
-              placeholder="비밀번호를 다시 입력하세요."></input>
+              className="w-80 mb-2 h-12"
+              type="password"
+              placeholder="비밀번호를 입력하세요."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div className="w-80 mb-2 flex">
-            <input className="w-60 mb-2" type="text" placeholder="비밀번호를 입력하세요."></input>
-            <button className="w-20 ml-4 text-center bg-primary text-white">중복확인</button>
-          </div>
-          <div className="mb-2">
+          <div className="mb-2 h-12">
             <input
-              className="w-80 mb-2"
+              className="w-80 mb-2 h-12"
+              type="password"
+              placeholder="비밀번호를 다시 입력하세요."
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
+          </div>
+          <div className="mb-2 h-12">
+            <input
+              className="w-80 mb-2 h-12"
               type="text"
-              placeholder="사용하실 닉네임을 입력하세요."></input>
+              placeholder="사용하실 닉네임을 입력하세요."
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
           </div>
           <div className="mb-2">
             <textarea
               className="w-80 mb-2 h-40 resize-none"
-              placeholder="소개를 입력하세요."></textarea>
+              placeholder="소개를 입력하세요."
+              value={introduction}
+              onChange={(e) => setIntroduction(e.target.value)}></textarea>
           </div>
           <div className="mb-2">
             <button className="w-80 h-10 bg-primary text-white" onClick={() => navigateTo('/')}>
