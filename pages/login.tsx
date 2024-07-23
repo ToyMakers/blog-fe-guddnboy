07264b336db6ddbc76215eed1b995a82de4308ff
guddnboy <hung3018@naver.com>
2024-07-23 19:03:51 +0900

    style(로그인 UI 크기 수정)

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,22 +3,24 @@ import { useRouter } from 'next/router';
 
 const Login = () => {
   const router = useRouter();
-  const navigateTo = (path: string) => {
-    router.push(path);
+
+  const navigateToSignUp = () => {
+    console.log('회원가입 페이지로 이동');
+    router.push('/signup');
   };
 
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className='w-80 h-12 mb-4'>
-          <input className="w-80 h-12" type="text" placeholder="ID" />
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <div>
+          <input className="w-60 h-14 my-3" type="text" placeholder="ID" />
         </div>
-        <div className='w-80 h-12 mb-4'>
-          <input className="w-80 h-12" type="password" placeholder="Password" />
+        <div>
+          <input className="w-60 h-14 my-3" type="password" placeholder="Password" />
         </div>
-        <div className="w-60 h-12 flex justify-between">
-          <button className="w-30 h-12 ">로그인</button>
-          <button className="w-30 h-12" onClick={() => navigateTo('/signup')}>
+        <div className="w-60 h-14 bg-white">
+          <button className="w-30 h-14 px-8">로그인</button>
+          <button className="w-30 h-14 px-8" onClick={navigateToSignUp}>
             회원가입
           </button>
         </div>
