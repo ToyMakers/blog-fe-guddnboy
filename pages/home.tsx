807716e5132a832785eb86fe620315b3c7719c4f
guddnboy <hung3018@naver.com>
2024-08-05 21:36:02 +0900

    design(글 항목 스타일 수정)

--- pages/home.tsx
+++ pages/home.tsx
@@ -57,15 +57,17 @@ const home = () => {
       </section>
       <section className="mt-16 mx-10">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-4 mr-4">
-          <div className="w-[240px] h-[240px] content transition hover:-translate-y-2 hover:cursor-pointer shadow-md rounded-sm">
-            <li className="flex flex-col w-[240px] h-[240px] text-titleColor border-2">
-              <a className="flex justify-center text-center w-full h-[32px] border-b-2 text-[18px]">
+          <div className="w-[280px] h-[280px] content transition hover:-translate-y-2 hover:cursor-pointer shadow-md rounded-md bg-white">
+            <li className="flex flex-col w-[280px] h-[280px] text-titleColor">
+              <a className="flex justify-center text-center w-full h-[32px] border-b-[1px] border-b-slate-200 text-[18px]">
                 블로그 제목 표시
               </a>
-              <div className="text-[16px] h-[170px] m-2">
+              <div className="text-[16px] h-[210px] m-2">
                 이 부분에 게시글 내용이 표시될 예정입니다.
               </div>
-              <footer className="text-left ml-4 text-[14px]">작성자</footer>
+              <footer className="w-full text-left pl-4 text-[14px] border-t-[1px] border-t-slate-200">
+                <div className="flex items-center">by 작성자</div>
+              </footer>
             </li>
           </div>
         </ul>
