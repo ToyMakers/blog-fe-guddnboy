d0fcf401f9a679863ca88a82559819cf9f3ce18c
guddnboy <hung3018@naver.com>
2024-08-05 22:07:38 +0900

    design(로그아웃,포스팅 이미지 추가)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
+import logoutbtn from '../public/assets/logoutbtn.png';
+import postbtn from '../public/assets/postbtn.png';
 
 interface HeaderProps {
   nickname: string;
@@ -8,26 +11,26 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ nickname, logout, navigateTo }) => {
   return (
-    <header className="flex justify-end w-full mt-4">
-      <ul className="flex justify-center mr-4">
-        <li className="mr-4">
-          <span
-            className="hover:cursor-pointer hover:text-gray-600 transition"
+    <header className="flex justify-end w-full">
+      <ul className="flex justify-center items-center">
+        <li className="flex flex-row mr-4">
+          <div
+            className="hover:cursor-pointer hover:text-gray-600 hover:font-bold  hover:-translate-y-1.5 transition"
             onClick={() => navigateTo('/mypage')}>
             {nickname}
-          </span>
-          <span> 님, 환영합니다.</span>
+          </div>
+          <span>님, 환영합니다</span>
         </li>
-        <li className="mr-4">
-          <button onClick={logout}>Logout</button>
+        <li className="mr-4 hover:bg-slate-200 hover:cursor-pointer hover:text-white transition rounded-md">
+          <Image src={logoutbtn} alt="logout" onClick={logout} className="w-[28px] h-[28px]" />
         </li>
-        <li className="mr-4">
-          <button
-            onClick={() => {
-              navigateTo('/write');
-            }}>
-            글쓰기
-          </button>
+        <li className="mr-4 hover:bg-slate-200 hover:cursor-pointer hover:text-white transition rounded-md">
+          <Image
+            src={postbtn}
+            alt="post"
+            onClick={() => navigateTo('/post')}
+            className="w-[28px] h-[28px]"
+          />
         </li>
       </ul>
     </header>
