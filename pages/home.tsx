8d753358903d7e6188a3e44b6f54e308961f8853
guddnboy <hung3018@naver.com>
2024-08-05 21:40:30 +0900

    design(제목, footer height 수정, 수직 가운데 정렬)

--- pages/home.tsx
+++ pages/home.tsx
@@ -59,14 +59,14 @@ const home = () => {
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-4 mr-4">
           <div className="w-[280px] h-[280px] content transition hover:-translate-y-2 hover:cursor-pointer shadow-md rounded-md bg-white">
             <li className="flex flex-col w-[280px] h-[280px] text-titleColor">
-              <a className="flex justify-center text-center w-full h-[32px] border-b-[1px] border-b-slate-200 text-[18px]">
+              <div className="flex justify-center items-center text-center w-full h-[48px] border-b-[1px] border-b-slate-200 text-[18px]">
                 블로그 제목 표시
-              </a>
+              </div>
               <div className="text-[16px] h-[210px] m-2">
                 이 부분에 게시글 내용이 표시될 예정입니다.
               </div>
-              <footer className="w-full h-[48px] text-left pl-4 text-[14px] border-t-[1px] border-t-slate-200">
-                <div className="flex items-center">by 작성자</div>
+              <footer className="flex items-center w-full h-[48px] text-left pl-4 text-[14px] border-t-[1px] border-t-slate-200">
+                <div>by 작성자</div>
               </footer>
             </li>
           </div>
