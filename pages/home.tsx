2f4ee5699e5f96ca8729e3282950a02a98f7d153
guddnboy <hung3018@naver.com>
2024-08-21 16:53:19 +0900

    rename: PostCard

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Header from '../components/Header';
-import BlogCard from '../components/BlogCard';
+import PostCard from '../components/PostCard';
 import Footer from '../components/Footer';
 
 const home = () => {
@@ -47,24 +47,33 @@ const home = () => {
     fetchProfile();
   }, [setNickname, router]);
 
+  const getPosts = async () => {
+    const token = localStorage.getItem('access_token');
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/posts`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('포스트를 가져오는 데 실패했습니다.');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      alert('포스트를 가져오는 데 실패했습니다.');
+    }
+  };
+
   return (
     <div className="mx-10 mt-8">
       <Header nickname={nickname ?? ''} logout={logout} navigateTo={navigateTo} />
       <section className="mt-16 flex-1">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
+          <PostCard />
         </ul>
       </section>
       <Footer />
