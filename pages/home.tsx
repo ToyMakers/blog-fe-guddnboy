1cebe59ca525a70e11a377cdcfe289899419a852
guddnboy <hung3018@naver.com>
2024-08-22 15:30:48 +0900

    feat: 전체 포스트 불러오기

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,10 +3,12 @@ import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import PostCard from '../components/PostCard';
 import Footer from '../components/Footer';
+import userStore from '../store/userStore';
 
 const home = () => {
   const router = useRouter();
-  const [nickname, setNickname] = useState<string | null>(null);
+  const { nickname, setNickname } = userStore();
+  const [postList, setPostList] = useState([]);
 
   const navigateTo = (path: string) => {
     router.push(path);
@@ -17,34 +19,37 @@ const home = () => {
     navigateTo('/login');
   };
 
-  useEffect(() => {
+  const fetchProfile = async () => {
     const token = localStorage.getItem('access_token');
-    const fetchProfile = async () => {
-      if (!token) {
-        alert('로그인이 필요합니다.');
-        router.push('/login');
-        return;
+
+    if (!token) {
+      alert('로그인이 필요합니다.');
+      router.push('/login');
+      return;
+    }
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/users/profile`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('프로필을 가져오는 데 실패했습니다.');
       }
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/users/profile`, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        });
-        if (!response.ok) {
-          throw new Error('프로필을 가져오는 데 실패했습니다.');
-        }
-        const data = await response.json();
-        setNickname(data.nickname);
-      } catch (error) {
-        console.error(error);
-        alert('프로필을 가져오는 데 실패했습니다.');
-        navigateTo('/login');
-      }
-    };
+      const data = await response.json();
+      setNickname(data.nickname);
+    } catch (error) {
+      console.error(error);
+      alert('프로필을 가져오는 데 실패했습니다.');
+      navigateTo('/login');
+    }
+  };
+
+  useEffect(() => {
     fetchProfile();
+    getPosts();
   }, [setNickname, router]);
 
   const getPosts = async () => {
@@ -61,7 +66,9 @@ const home = () => {
         throw new Error('포스트를 가져오는 데 실패했습니다.');
       }
       const data = await response.json();
-      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        setPostList(data);
+      }
     } catch (error) {
       console.error(error);
       alert('포스트를 가져오는 데 실패했습니다.');
@@ -73,7 +80,12 @@ const home = () => {
       <Header nickname={nickname ?? ''} logout={logout} navigateTo={navigateTo} />
       <section className="mt-16 flex-1">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
-          <PostCard />
+          {postList
+            .slice()
+            .reverse()
+            .map((post, index) => (
+              <PostCard key={index} post={post} />
+            ))}
         </ul>
       </section>
       <Footer />
