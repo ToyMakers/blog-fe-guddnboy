52d19c7812632832e2be88986e9854ae0ee8cba7
guddnboy <hung3018@naver.com>
2024-08-27 15:15:23 +0900

    feat: postStore, postListStore 추가

--- store/postStore.tsx
+++ store/postStore.tsx
@@ -4,17 +4,26 @@ interface PostStoreProps {
   title: string;
   content: string;
   category: string;
+  id: string;
+  author: string;
   setTitle: (title: string) => void;
   setContent: (content: string) => void;
   setCategory: (category: string) => void;
+  setId: (id: string) => void;
+  setAuthor: (author: string) => void;
 }
 
 const postStore = create<PostStoreProps>((set) => ({
   title: '',
   content: '',
   category: '',
+  id: '',
+  author: '',
   setTitle: (title) => set({ title }),
   setContent: (content) => set({ content }),
   setCategory: (category) => set({ category }),
+  setId: (id) => set({ id }),
+  setAuthor: (author) => set({ author }),
 }));
+
 export default postStore;
