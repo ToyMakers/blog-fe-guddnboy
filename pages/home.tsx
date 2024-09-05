6081ae98e20a10ed80be72d5aa25998d41f430d9
guddnboy <hung3018@naver.com>
2024-09-05 17:19:30 +0900

    refact: router.query에서 zustand로 상태관리 방법 변경

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import PostCard from '../components/PostCard';
 import Footer from '../components/Footer';
 import userStore from '../store/userStore';
 import postListStore from '../store/postListStore';
+import postStore from '@/store/postStore';
 
 const home = () => {
   const router = useRouter();
   const { nickname, setNickname } = userStore();
   const { postList, setPostList } = postListStore();
+  const setPostData = postStore((state) => state.setPostData) as (data: {
+    id: string;
+    title: string;
+    content: string;
+    author: string;
+    likes: number;
+    categories: string[];
+  }) => void;
 
   const navigateTo = (path: string) => {
     router.push(path);
@@ -50,6 +59,7 @@ const home = () => {
 
   const getPosts = async () => {
     const token = localStorage.getItem('access_token');
+
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/posts`, {
         method: 'GET',
@@ -58,50 +68,47 @@ const home = () => {
           'Content-Type': 'application/json',
         },
       });
+
+      console.log(response);
       if (!response.ok) {
-        throw new Error('포스트를 가져오는 데 실패했습니다.');
+        throw new Error('포스트를 가져오는 데 실패했습니다.: response.ok가 false입니다.');
       }
       const data = await response.json();
       setPostList(data);
     } catch (error) {
       console.error(error);
-      alert('포스트를 가져오는 데 실패했습니다.');
+      alert('포스트를 가져오는 데 실패했습니다.: catch문 실행됨');
     }
   };
 
   const handlePostClick = (post: any) => {
-    router.push({
-      pathname: '/postdetail',
-      query: {
-        nickname: nickname,
-
-        postTitle: post.title,
-        postContent: post.content,
-        postAuthor: post.author,
-        postLikes: post.likes,
-        postCategories: post.categories,
-      },
+    setPostData({
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      author: post.author,
+      likes: post.likes,
+      categories: post.categories,
     });
+
+    router.push(`/postdetail`);
   };
 
   useEffect(() => {
     fetchProfile();
     getPosts();
-  }, [nickname, router]);
+  }, []);
 
   return (
     <div className="mx-10 mt-8">
       <Header nickname={nickname ?? ''} logout={logout} navigateTo={navigateTo} />
       <section className="mt-16 flex-1">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
-          {postList
-            .slice()
-            .reverse()
-            .map((post, index) => (
-              <li key={index} onClick={() => handlePostClick(post)}>
-                <PostCard post={post} />
-              </li>
-            ))}
+          {postList.map((post, index) => (
+            <li key={index} onClick={() => handlePostClick(post)}>
+              <PostCard post={post} />
+            </li>
+          ))}
         </ul>
       </section>
       <Footer />
