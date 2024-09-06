8a16b106f0963d9a01b82db16d6936c84d552bcb
guddnboy <hung3018@naver.com>
2024-09-06 12:39:11 +0900

    refact: 포스트카드에서 제목, 내용 넘쳐나면 hidden 적용

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -4,10 +4,10 @@ const PostCard = ({ post }: { post: any }) => {
   return (
     <div className="gap-4">
       <li className="blog-card flex flex-col w-full h-full text-titleColor">
-        <div className="flex justify-center items-center text-center w-full h-[48px] border-b-[1px] border-b-slate-200 text-[18px]">
+        <div className="flex justify-center items-center text-center w-full h-[48px] border-b-[1px] border-b-slate-200 text-[18px] overflow-hidden">
           {post.title}
         </div>
-        <div className="text-[16px] h-[210px] m-2">{post.content}</div>
+        <div className="text-[16px] h-[210px] m-2 overflow-hidden">{post.content}</div>
         <footer className="flex items-center justify-between w-full h-12 text-left p-4 text-[14px] border-t-[1px] border-t-slate-200">
           <div className="pr-4">by {post.author}</div>
           <div className="flex items-center gap-1">
