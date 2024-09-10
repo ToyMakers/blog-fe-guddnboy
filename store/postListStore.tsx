9f2a893d8218cbfbadc5bfe69c6bef4ee3521e6a
guddnboy <hung3018@naver.com>
2024-09-10 16:37:45 +0900

    refact: 필요없는 코드 제거

--- store/postListStore.tsx
+++ store/postListStore.tsx
@@ -1,16 +1,22 @@
 import { create } from 'zustand';
 
-type State = {
-  postList: [];
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+  categories: string[];
+  author: string;
+  likes: number;
 };
 
-type Actions = {
-  setPostList: (postList: []) => void;
+type PostListState = {
+  postList: Post[];
+  setPostList: (postList: Post[]) => void;
 };
 
-const postListStore = create<State & Actions>((set) => ({
+const postListStore = create<PostListState>((set) => ({
   postList: [],
-  setPostList: (postList: []) => set({ postList }),
+  setPostList: (postList: Post[]) => set({ postList }),
 }));
 
 export default postListStore;
